59f388f5f2ec5ae08c13e68af94d88492bd03c0d
CodeDestroyer69 <95127615+CodeDestroyer69@users.noreply.github.com>
2021-11-27 13:53:46 +0200

    Add files via upload

---
+++ expressions.java
@@ -0,0 +1,11 @@
+public class expressions {
+    public static void main(String[] args) {
+        //operators = operands & operators 
+        //operands = values, vriabiles, numbers, quantity
+        //operators= + / * - %
+
+        int firends = 10;
+            firends = firends + 1;
+                System.out.println(firends);
+    }
+}
