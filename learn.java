d62d281a91ce2dfea47cbbe63765076cc71a4c97
CodeDestroyer69 <95127615+CodeDestroyer69@users.noreply.github.com>
2021-11-27 13:13:33 +0200

    Add files via upload

---
+++ learn.java
@@ -0,0 +1,20 @@
+public class learn {
+    public static void main(String[] args) {
+        //int x; //declaration 
+
+        //x=123; //assignement 
+
+        int x=123; //initialization
+                    // poate stoca nmera pana la 2 bilioane 
+            System.out.println(x);
+        
+        long y=1281418414342424L; // poate stoca numere pana la 9 quintillion + trebuie pus L la final ca sa functioneze 
+            System.out.println(y);
+
+        double z=31313.31233; //double este folosti pentru numere fractionale 
+            System.out.println(z);
+
+        System.out.println("My number is " + x); //arata propozitia "My number is " si adauga la final variabila x 
+
+    }
+}
